addfb74d6065340d868369d51af02eca601aba15
lukeschula <schulaluke@gmail.com>
2025-01-30 00:46:31 -0600

    move test

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,13 +1,13 @@
 import '@testing-library/jest-dom/vitest';
 import { afterAll, beforeAll, afterEach } from 'vitest';
-import { server } from './src/mocks/handlers';
+import { server } from './src/mocks/handlers.ts';
 
 
-//Establish API mocking
+
 beforeAll(() => server.listen());
 
-//Reset request after each test
+
 afterEach(() => server.resetHandlers());
 
-//Cleanup after tests are finished
+
 afterAll(() => server.close());
